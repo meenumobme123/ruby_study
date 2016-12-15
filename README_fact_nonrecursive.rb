aba752a1d810c03cecc46ce4c9a07f83a42b9d31
meenu <meenu.rajan@mobme.in>
2016-12-15 17:48:41 +0530

    factorial

---
+++ README_fact_nonrecursive.rb
@@ -0,0 +1,5 @@
+This is a program for finding factorial of 5 by non-recursive method
+
+How to run :ruby fact_nonrecursive.rb
+
+Dependencies :Ruby
---
+++ README_fact_recursive.rb
@@ -0,0 +1,5 @@
+This is a program for finding factorial of 5 by recursive method
+
+How to run :ruby fact_recursive.rb
+
+Dependencies :Ruby
